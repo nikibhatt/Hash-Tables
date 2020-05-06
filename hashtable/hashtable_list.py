2a2962de510430dbf38639ade8346c4667210a27
nikibhatt <nkibhatt@users.noreply.github.com>
2020-05-05 17:06:43 -0700

    completed day 2 assignment

--- hashtable/hashtable_list.py
+++ hashtable/hashtable_list.py
@@ -55,19 +55,7 @@ class HashTable:
         Implement this.
         """
         index = self.hash_index(key)
-        n = HashTableEntry(key, value)
-        if self.hash_table[index] is None:
-            self.hash_table[index] = n
-            return
-        head = self.hash_table[index]
-        cur = head
-        while cur is not None:
-            if cur.key == key:
-                cur.value = value
-                return
-            cur = cur.next
-        n.next = head
-        self.hash_table[index] = n
+        self.hash_table[index] = (key,value)
 
 
     def delete(self, key):
@@ -79,23 +67,7 @@ class HashTable:
         Implement this.
         """
         index = self.hash_index(key)
-        if self.hash_table[index] is None:
-            return
-
-        head = self.hash_table[index]
-        if head.key == key:
-            head = head.next
-            self.hash_table[index] = head
-            return
-
-        cur = head
-        while cur.next is not None:
-                prev = cur
-                cur = cur.next
-                if cur.key == key:
-                    prev.next = cur.next
-                    return
-        return
+        self.hash_table[index] = None
 
     def get(self, key):
         """
@@ -106,13 +78,10 @@ class HashTable:
         Implement this.
         """
         index = self.hash_index(key)
-        head = self.hash_table[index]
-        cur = head
-        while cur is not None:
-            if cur.key == key:
-                return cur.value
-            cur = cur.next
-        return None
+        value = None
+        if self.hash_table[index] is not None:
+            key, value = self.hash_table[index]
+        return value
 
     def resize(self):
         """
@@ -124,8 +93,9 @@ class HashTable:
         new_hash_table = [None] * 2 * self.capacity
         for i in range(len(self.hash_table)):
             if self.hash_table[i] is not None:
-                new_index = self.hash_index(self.hash_table[i].key)
-                new_hash_table[new_index] = self.hash_table[i]
+                key, value = self.hash_table[i]
+                new_index = self.hash_index(key)
+                new_hash_table[new_index] = key, value
         self.hash_table = new_hash_table
 
 if __name__ == "__main__":
